dd6597460938bbdaaaa01f812f2a27b0d3bded63
Tevin Glover <tevwork.tg@gmail.com>
2024-01-28 20:00:45 -0500

    start working on the last 3 test

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -144,5 +144,38 @@ public void classNullaryConstructorTest() {
 
     }
 
+  /** Define method removeStudent(String firstName, String lastName)
+    The class Classroom should define a method which uses a firstName and lastName parameter to identify and remove the respective student from composite students object.
+    Ensure the array is re-ordered after the removal; Null values should be located in the final indices of the array.
+**/
 
-}
+  @Test
+    public void removeStudentTest(){
+
+  }
+
+
+
+/**    Define method getStudentsByScore()
+  The class Classroom should define a method getStudentsByScore() which returns an array representation of Student objects sorted in descending order by score.
+    If two students have the same class average, order them lexigraphically.
+**/
+
+//@Test
+//public void getStudentsByScoreTest(){
+//
+//}
+
+/**    Define method getGradeBook()
+    The class Classroom should define a method getGradeBook() which returns a mapping of Student objects to a respective letter grade determined by creating a grading curve such that
+    An A is awarded to students whose class average is in the upper 10th percentile.
+    A B is awarded to students whose class average falls between the upper 11th and 29th percentile.
+    A C is awarded to students whose class average falls between the upper 30th and 50th percentile.
+    A D is awarded to students whose class average falls between the lower 51st and 89th percentile.
+    An F is awarded to students whose class average is in the lower 11th percentile.
+**/
+//@Test
+//public void getGradeBookTest(){
+//
+//}
+ }
